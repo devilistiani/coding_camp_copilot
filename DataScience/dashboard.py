3a3acbc1210217a4ed27052c00ff639f989e44d0
Devi Listiani Safitri <156830243+devilistiani@users.noreply.github.com>
2026-05-31 22:03:42 +0700

    Update load_data function to use Path for file paths
    Refactor data loading to use base directory path for CSV files.

--- DataScience/dashboard.py
+++ DataScience/dashboard.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
+from pathlib import Path
 
 
 
@@ -110,25 +111,27 @@ st.markdown(
 )
 st.markdown("---")
 
-
 @st.cache_data
 def load_data():
-    df_bersih = pd.read_csv("data/data_bersih.csv")
-    df_ab_test = pd.read_csv("data/hasil_ab_testing.csv")
-    df_tfidf = pd.read_csv("data/dataset_tfidf.csv")
+    base_dir = Path(__file__).parent
 
-  # filter path yang tidak dipakai di dataset final  
-    df_bersih = df_bersih.rename(columns={"Kategori_Bersih": "Path"})
+    df_bersih = pd.read_csv(base_dir / "data" / "data_bersih.csv")
+    df_ab_test = pd.read_csv(base_dir / "data" / "hasil_ab_testing.csv")
+    df_tfidf = pd.read_csv(base_dir / "data" / "dataset_tfidf.csv")
 
+    df_bersih = df_bersih.rename(
+        columns={"Kategori_Bersih": "Path"}
+    )
 
     df_bersih = df_bersih[
-        ~df_bersih["Path"].str.strip().str.lower().isin(["tidak diketahui", "unknown", ""])
+        ~df_bersih["Path"].str.strip().str.lower().isin(
+            ["tidak diketahui", "unknown", ""]
+        )
     ]
+
     df_bersih = df_bersih.dropna(subset=["Path"])
 
     return df_bersih, df_ab_test, df_tfidf
-
-df_bersih, df_ab_test, df_tfidf = load_data()
 
 
 @st.cache_resource
